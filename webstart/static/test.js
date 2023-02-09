882ae9b23155b1bbf479727796e7cc1186b32cc9
RGCSERG <sergentrufus@gmail.com>
2023-02-09 21:32:01 +0000

    fixed test methods

--- webstart/static/test.js
+++ webstart/static/test.js
@@ -1,4 +1,11 @@
 let text = document.getElementById('text');
-let case = document.getElementById('bg');
+let case2 = document.getElementById('case2');
+let case1 = document.getElementById('case');
 
-window.addEventListener('scroll')
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+
+    text.style.marginTop = value * 2.5 + 'px';
+    case2.style.marginTop = value * -1.5 + 'px';
+    case1.style.marginTop = value * -1.5 + 'px';
+})
