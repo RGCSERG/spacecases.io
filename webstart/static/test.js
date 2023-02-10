8f243734e2b0f731b9a2ef342d542438c61287f3
thomasyonaha-mccoy <83908974+goat6@users.noreply.github.com>
2023-02-10 22:32:32 +0000

    more js adjustments & changed background

--- webstart/static/test.js
+++ webstart/static/test.js
@@ -8,6 +8,10 @@ window.addEventListener('scroll', () => {
     text.style.marginTop = value * 2.5 + 'px';
     case1.style.left = value * -1.5 + 'px';
     case2.style.left = value * 1.5 + 'px';
+    //case2.style.opacity = 1 - (value*0.001)*2;
+    //case1.style.opacity = 1 - (value*0.01)*2;
+    text.style.opacity = 1 - (value * 0.001) * 2;
+    console.log(value)
 })
 
 const menu = document.querySelector(".burger-menu");
