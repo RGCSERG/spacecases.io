f870db7106edd5035b99d889600e3c706e0451d3
RGCSERG <sergentrufus@gmail.com>
2023-02-24 20:55:29 +0000

    changes

--- webstart/__init__.py
+++ webstart/__init__.py
@@ -6,6 +6,6 @@ import webstart.database_commands as db
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'
 Client = APIClient(TOKEN, client_secret=CLIENT_SECRET)
-db.init()
+#db.init()
 
 from webstart import routes
