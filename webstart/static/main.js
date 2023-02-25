e2872a649bcd2cbfc7f4f83ca8da07b886829965
thomasyonaha-mccoy <83908974+goat6@users.noreply.github.com>
2023-02-25 22:26:21 +0000

    updated lb page added cycling between lb

--- webstart/static/main.js
+++ webstart/static/main.js
@@ -13,6 +13,33 @@ function setUpEvents() {
     })
 }
 
+function leaderboardCycle() { 
+    var arrows = document.querySelectorAll(".lb_svg")
+    var count = 1
+    var text = document.getElementById("lbtext")
+    var globalLB = document.querySelector(".global")
+    var serverLB = document.querySelector(".server")
+
+    for (var i = 0; i < arrows.length; i++) {
+        arrows[i].addEventListener("click", function () {
+            if (count == 1) {
+                count = 2
+                serverLB.style.visibility = "visible";
+                globalLB.style.visibility = "hidden";
+                text.innerHTML = "Server Leaderboard";
+            } else {
+                count = 1
+                text.innerHTML = "Global Leaderboard";
+                serverLB.style.visibility = "hidden";
+                globalLB.style.visibility = "visible";
+            }
+            console.log(count)
+            
+        })
+    }
+    
+}
 window.onload = function(){ 
     setUpEvents();
+    leaderboardCycle();
 }
