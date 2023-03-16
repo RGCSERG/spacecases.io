e27763fec04fd74dc7b8a15b380fc02c97ad0260
RGCSERG <sergentrufus@gmail.com>
2023-03-16 13:05:51 +0000

    changed secret key

--- webstart/__init__.py
+++ webstart/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from zenora import APIClient
-from webstart.config import CLIENT_SECRET, TOKEN
+from webstart.config import CLIENT_SECRET, TOKEN , SECRET_KEY
 import webstart.database_commands as db
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secretkey'
+app.config['SECRET_KEY'] = SECRET_KEY
 Client = APIClient(TOKEN, client_secret=CLIENT_SECRET)
+
 db.init()
 
 from webstart import routes
