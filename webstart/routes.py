97e6f47996f2241835127ff5f4b5bcbe564023ad
RGCSERG <sergentrufus@gmail.com>
2023-03-28 09:59:43 +0100

    removing cookies as do not work

--- webstart/routes.py
+++ webstart/routes.py
@@ -7,11 +7,12 @@ from .config import REDIRECT_URI, OAUTH_URL, CLIENT_SECRET, TOKEN, INVITE_URL
 from .calculations import permissions, iscasesin, updateLD
 from flask import render_template, url_for, flash, redirect, request, session, make_response
 from datetime import timedelta, datetime
+#add if statement for invite server
 #check if current_user in session works -- can confirm you can't add current_user to json file as it is a class
 
 @app.route('/')
-def check_for_user():
-    return redirect('/get')
+# def check_for_user():
+#     return redirect('/get')
 
 
 @app.route('/home')
@@ -33,10 +34,10 @@ def callback():
     session['token'] = resp.access_token
     session['refresh_token'] = resp.refresh_token
     session.permanent = True
-    cookie_set = make_response(redirect('/home'))
-    cookie_set.set_cookie('tokens', resp.access_token + ':' + resp.refresh_token + ':' + resp.scope  + ':' + resp.token_type)
+    # cookie_set = make_response(redirect('/home'))
+    # cookie_set.set_cookie('tokens', resp.access_token + ':' + resp.refresh_token + ':' + resp.scope  + ':' + resp.token_type)
 
-    return cookie_set
+    return redirect('/home')
 
 
 @app.route('/leaderboard')
@@ -73,25 +74,23 @@ def logout():
     session.clear()
     return redirect("/")
 
-@app.route('/get')
-def getcookie():
-    if request.cookies.get('tokens'):
-        refresh_token = request.cookies.get('tokens').split(':')[1]
-        access_token = request.cookies.get('tokens').split(':')[0]
-        print('check')
-        resp = Client.oauth.refresh_access_token(refresh_token)
-        session['token'] = resp.access_token
-        try:
-            print('check')
-            resp = Client.oauth.refresh_access_token(refresh_token)
-            session['token'] = resp.access_token
-        except KeyError:
-            return redirect('/home')
-        try:
-            session['token'] = access_token
-            bearer_client = APIClient(session.get('token'), bearer=True)#
-        except BadTokenError:
-            return redirect('/home')
+# @app.route('/get')
+# def getcookie():
+#     if request.cookies.get('tokens'):
+#         refresh_token = request.cookies.get('tokens').split(':')[1]
+#         access_token = request.cookies.get('tokens').split(':')[0]
+#         resp = Client.oauth.refresh_access_token(refresh_token)
+#         session['token'] = resp.access_token
+#         try:
+#             resp = Client.oauth.refresh_access_token(refresh_token)
+#             session['token'] = resp.access_token
+#         except KeyError:
+#             return redirect('/home')
+#         try:
+#             session['token'] = access_token
+#             bearer_client = APIClient(session.get('token'), bearer=True)#
+#         except BadTokenError:
+#             return redirect('/home')
 
 @app.route('/profile')
 def profile():
