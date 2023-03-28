7f07343205ceb345e10771b38c63f507c8ebf0be
RGCSERG <sergentrufus@gmail.com>
2023-03-28 11:11:26 +0100

    changed reate app to a function using config class

--- webstart/__init__.py
+++ webstart/__init__.py
@@ -1,7 +1,7 @@
 
 from flask import Flask
 from zenora import APIClient
-from .config import CLIENT_SECRET, TOKEN , SECRET_KEY
+from .config import CLIENT_SECRET, TOKEN , SECRET_KEY, Config
 import webstart.database_commands as db
 import os
 
@@ -9,6 +9,23 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 Client = APIClient(TOKEN, client_secret=CLIENT_SECRET)
 
-db.init()
 
-from webstart import routes
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init()
+
+    from .bot_management.routes import bot_management
+    from .errors.routes import errors
+    from .main.routes import main
+    from .oauth2.routes import oauth2
+    from .users.routes import users
+
+    app.register_blueprint(bot_management)
+    app.register_blueprint(errors)
+    app.register_blueprint(main)
+    app.register_blueprint(oauth2)
+    app.register_blueprint(users)
+
+    return app
