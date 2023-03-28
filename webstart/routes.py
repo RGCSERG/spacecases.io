a71e0eecfb314d07373e4ca530298ab1c79e5831
RGCSERG <sergentrufus@gmail.com>
2023-03-28 09:52:34 +0100

    add token session expired try for check

--- webstart/routes.py
+++ webstart/routes.py
@@ -7,7 +7,6 @@ from .config import REDIRECT_URI, OAUTH_URL, CLIENT_SECRET, TOKEN, INVITE_URL
 from .calculations import permissions, iscasesin, updateLD
 from flask import render_template, url_for, flash, redirect, request, session, make_response
 from datetime import timedelta, datetime
-#add token session expired try for check
 #check if current_user in session works -- can confirm you can't add current_user to json file as it is a class
 
 @app.route('/')
@@ -17,10 +16,13 @@ def check_for_user():
 
 @app.route('/home')
 def home():
-    if 'token' in session:
-        bearer_client = APIClient(session.get('token'), bearer=True)
-        current_user = bearer_client.users.get_current_user()
-        return render_template('home.html', current_user=current_user)
+    try:
+        if 'token' in session:
+            bearer_client = APIClient(session.get('token'), bearer=True)
+            current_user = bearer_client.users.get_current_user()
+            return render_template('home.html', current_user=current_user)
+    except BadTokenError:
+        return render_template('home.html', oauth_url=OAUTH_URL)
     return render_template('home.html', oauth_url=OAUTH_URL)
 
 
@@ -40,22 +42,30 @@ def callback():
 @app.route('/leaderboard')
 def leaderboard():
     Leaderboard, status = updateLD(Client, get_leaderboard, db)
-    if 'token' in session:
-        bearer_client = APIClient(session.get('token'), bearer=True)
-        current_user = bearer_client.users.get_current_user()
-        guilds = iscasesin(bearer_client.users.get_my_guilds(), Client.users.get_my_guilds())
-        return render_template('leaderboard.html',oauth_url=OAUTH_URL,Leaderboard=Leaderboard,current_user=current_user,guilds=guilds,str=str,len=len,interate=status) # remove [:10] when updateLD is fixed and not using setlist
+    try:
+        if 'token' in session:
+            bearer_client = APIClient(session.get('token'), bearer=True)
+            current_user = bearer_client.users.get_current_user()
+            guilds = iscasesin(bearer_client.users.get_my_guilds(), Client.users.get_my_guilds())
+            return render_template('leaderboard.html',oauth_url=OAUTH_URL,Leaderboard=Leaderboard,current_user=current_user,guilds=guilds,str=str,len=len,interate=status) # remove [:10] when updateLD is fixed and not using setlist
+    except BadTokenError:
+        return render_template('leaderboard.html',oauth_url=OAUTH_URL,Leaderboard=Leaderboard,len=len,iterate=status) # remove [:10] when updateLD is fixed and not using setlist
     return render_template('leaderboard.html',oauth_url=OAUTH_URL,Leaderboard=Leaderboard,len=len,iterate=status) # remove [:10] when updateLD is fixed and not using setlist
+
 
 
 @app.route('/invite_server')
 def invite_server():
-    if 'token' in session:
-        bearer_client = APIClient(session.get('token'), bearer=True)
-        current_user = bearer_client.users.get_current_user()
-        guilds = permissions(bearer_client.users.get_my_guilds())
-        return render_template('invite_Server.html', current_user=current_user, guilds=guilds, invite_url=INVITE_URL, str=str)
+    try:
+        if 'token' in session:
+            bearer_client = APIClient(session.get('token'), bearer=True)
+            current_user = bearer_client.users.get_current_user()
+            guilds = permissions(bearer_client.users.get_my_guilds())
+            return render_template('invite_Server.html', current_user=current_user, guilds=guilds, invite_url=INVITE_URL, str=str)
+    except BadTokenError:
+        return render_template('invite_server.html', oauth_url=OAUTH_URL, invite_url=INVITE_URL)
     return render_template('invite_server.html', oauth_url=OAUTH_URL, invite_url=INVITE_URL)
+
 
 
 @app.route('/logout')
@@ -77,73 +87,95 @@ def getcookie():
             session['token'] = resp.access_token
         except KeyError:
             return redirect('/home')
-        # try:
-        #     session['token'] = access_token
-        #     bearer_client = APIClient(session.get('token'), bearer=True)#
-        # except BadTokenError:
-        #     return redirect('/home')
+        try:
+            session['token'] = access_token
+            bearer_client = APIClient(session.get('token'), bearer=True)#
+        except BadTokenError:
+            return redirect('/home')
 
 @app.route('/profile')
 def profile():
-    if 'token' in session:
-        bearer_client = APIClient(session.get('token'), bearer=True)
-        current_user = bearer_client.users.get_current_user()
-        return render_template('profile.html', current_user=current_user)
-
+    try:
+        if 'token' in session:
+            bearer_client = APIClient(session.get('token'), bearer=True)
+            current_user = bearer_client.users.get_current_user()
+            return render_template('profile.html', current_user=current_user)
+    except BadTokenError:
+        return render_template('profile.html', oauth_url=OAUTH_URL)
     return render_template('profile.html', oauth_url=OAUTH_URL)
+
 
 
 @app.route('/test')
 def test():
-    if 'token' in session:
-        bearer_client = APIClient(session.get('token'), bearer=True)
-        current_user = bearer_client.users.get_current_user()
-        return render_template('test.html', current_user=current_user)
-
+    try:
+        if 'token' in session:
+            bearer_client = APIClient(session.get('token'), bearer=True)
+            current_user = bearer_client.users.get_current_user()
+            return render_template('test.html', current_user=current_user)
+    except BadTokenError:
+        return render_template('test.html', oauth_url=OAUTH_URL)
     return render_template('test.html', oauth_url=OAUTH_URL)
 
 
 #|--ERROR HANDLING--|#
 @app.errorhandler(400)
-def page_not_found(e):
-    if 'token' in session:
-        bearer_client = APIClient(session.get('token'), bearer=True)
-        current_user = bearer_client.users.get_current_user()
-        return render_template('400.html', current_user=current_user)
+def fourhundred(e):
+    try:
+        if 'token' in session:
+            bearer_client = APIClient(session.get('token'), bearer=True)
+            current_user = bearer_client.users.get_current_user()
+            return render_template('400.html', current_user=current_user)
+    except BadTokenError:
+        return render_template('400.html', oauth_url=OAUTH_URL)
     return render_template('400.html', oauth_url=OAUTH_URL)
+
+    
 
 
 @app.errorhandler(401)
-def page_not_found(e):
-    if 'token' in session:
-        bearer_client = APIClient(session.get('token'), bearer=True)
-        current_user = bearer_client.users.get_current_user()
-        return render_template('401.html', current_user=current_user)
+def fourOone(e):
+    try:
+        if 'token' in session:
+            bearer_client = APIClient(session.get('token'), bearer=True)
+            current_user = bearer_client.users.get_current_user()
+            return render_template('401.html', current_user=current_user)
+    except BadTokenError:
+        return render_template('401.html', oauth_url=OAUTH_URL)
     return render_template('401.html', oauth_url=OAUTH_URL)
 
 
 @app.errorhandler(403)
-def page_not_found(e):
-    if 'token' in session:
-        bearer_client = APIClient(session.get('token'), bearer=True)
-        current_user = bearer_client.users.get_current_user()
-        return render_template('403.html', current_user=current_user)
+def fourOthree(e):
+    try:
+        if 'token' in session:
+            bearer_client = APIClient(session.get('token'), bearer=True)
+            current_user = bearer_client.users.get_current_user()
+            return render_template('403.html', current_user=current_user)
+    except BadTokenError:
+        return render_template('403.html', oauth_url=OAUTH_URL)
     return render_template('403.html', oauth_url=OAUTH_URL)
 
 
 @app.errorhandler(404)
-def page_not_found(e):
-    if 'token' in session:
-        bearer_client = APIClient(session.get('token'), bearer=True)
-        current_user = bearer_client.users.get_current_user()
-        return render_template('404.html', current_user=current_user)
+def fourOfour(e):
+    try:
+        if 'token' in session:
+            bearer_client = APIClient(session.get('token'), bearer=True)
+            current_user = bearer_client.users.get_current_user()
+            return render_template('404.html', current_user=current_user)
+    except BadTokenError:
+        return render_template('404.html', oauth_url=OAUTH_URL)
     return render_template('404.html', oauth_url=OAUTH_URL)
 
 
 @app.errorhandler(500)
-def page_not_found(e):
-    if 'token' in session:
-        bearer_client = APIClient(session.get('token'), bearer=True)
-        current_user = bearer_client.users.get_current_user()
-        return render_template('500.html', current_user=current_user)
+def Fivehundred(e):
+    try:
+        if 'token' in session:
+            bearer_client = APIClient(session.get('token'), bearer=True)
+            current_user = bearer_client.users.get_current_user()
+            return render_template('500.html', current_user=current_user)
+    except BadTokenError:
+        return render_template('500.html', oauth_url=OAUTH_URL)
     return render_template('500.html', oauth_url=OAUTH_URL)
